e1870c6d5781dc12f66acfeb465aec59cc32b74b
Benjamin Albanese <b.albanese@levigo.de>
2023-10-09 11:49:20 +0200

    replace the dummy frontend with a first, simple version of the viewer that is able to load documents.

--- tutorial-001/client/src/main.ts
+++ tutorial-001/client/src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {enableProdMode} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./root/app.module";
+import {environment} from "./environments/environment";
+import {preloadPrecursor} from "@levigo/webtoolkit-ng-client";
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const serverURL = environment.production ? window.location.origin : window.location.origin + "/api";
+
+// This part is absolutely necessary as it will bottstrap the angular-GWT wrapper called "precursor"
+// As of the start of jwv 6.x, it is planned to reduce that layer step by step so in the end we
+// might end up without any GWT code, but for a start we still need some of the old jadice web toolkit 5 part.
+preloadPrecursor({serverURL}).then(() => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+        .catch(err => console.error(err));
+});
