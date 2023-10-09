39795ab135c91cd5ae05f4ce8b9fd0541ab06d5a
Benjamin Albanese <b.albanese@levigo.de>
2023-10-09 11:50:42 +0200

    add more features to the viewer

--- tutorial-001/client/src/root/app.component.ts
+++ tutorial-001/client/src/root/app.component.ts
@@ -1,9 +1,33 @@
-import {Component, ViewChild} from "@angular/core";
-import {DefaultToolbar, DocumentSource, Viewer} from "@levigo/webtoolkit-ng-client";
-import {MultiModeViewerComponent} from "@levigo/ngx-webtoolkit";
-import {ToolbarConfig} from "@levigo/jadice-common-components";
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {
+    AnnotationCustomizers,
+    AnnotationInstanceType,
+    AnnotationProfile,
+    AnnotationProfileCache,
+    AnnotationProfileUtils, DefaultActions,
+    DefaultToolbar,
+    DocumentAnnotations,
+    DocumentSource,
+    GWTDocumentWrapper,
+    GWTImageAnnotationWrapper,
+    Viewer,
+    ViewerType
+} from "@levigo/webtoolkit-ng-client";
+import {
+    MultiModeViewerComponent,
+    OpenFileTemplate,
+    ThumbnailPanelComponent,
+    UploadDialogsWrapperComponent
+} from "@levigo/ngx-webtoolkit";
+import {BehaviorSubject, map, tap} from "rxjs";
+import {MenuItemType, ToolbarConfig, ToolbarUtils} from "@levigo/jadice-common-components";
 import {Nullable} from "@levigo/utility-types";
 import {I18NService} from "@levigo/ngx-translate-support";
+import {FLOATING_BUTTON_CONFIG} from "./config/floating-button-config";
+import {DEMO_DOCUMENTS} from "./config/demo-documents";
+import {PILLBOX_CONFIG} from "./config/pillbox-config";
+import {SWITCH_MODE_ACTION} from "./config/switch-mode-action";
+import {I18N} from "@levigo/jadice-i18n-support";
 
 // @ts-ignore
 @Component({
@@ -12,21 +36,157 @@ import {I18NService} from "@levigo/ngx-translate-support";
     styleUrls: ['./app.component.scss'],
     standalone: false // for a proper fix, see https://v17.angular.io/guide/standalone-components
 })
-export class AppComponent {
-    readonly TOOLBAR_CONFIG: ToolbarConfig<Viewer>;
+export class AppComponent implements OnInit{
+    readonly DEFAULT_PROFILE = "JWT-Demo-Profile";
+
+    // The following variables' values are those classes that are defined in the subfolder "config"
+    readonly DEMO_DOCUMENTS = DEMO_DOCUMENTS;
+    readonly FLOATING_BUTTON_CONFIG = FLOATING_BUTTON_CONFIG;
+    readonly PILLBOX_CONFIG = PILLBOX_CONFIG;
+
+    // viewer mode - default mode or accessible mode for people with visual impairment?
+    mode$ = new BehaviorSubject<boolean>(false);
+    vieweType$ = this.mode$.pipe(map(mode => (mode ? ViewerType.ACCESSIBLE : ViewerType.RENDERED_GWT)));
+
+    // use the default config and add the button for switching the view mode (normal/accessible)
+    TOOLBAR_CONFIG: ToolbarConfig<Viewer>;
 
     @ViewChild("viewerComponent", {static: true})
     viewerComponent!: MultiModeViewerComponent;
 
+    @ViewChild("thumbnailPanelComponent")
+    thumbnailPanelComponent!: ThumbnailPanelComponent;
+
+    @ViewChild("uploadDialogsWrapper")
+    uploadDialogsWrapper!: UploadDialogsWrapperComponent;
+
+    currentDocument: GWTDocumentWrapper | null = null;
+
+    passwordRequiredSource: DocumentSource | null = null;
+
     source: Nullable<DocumentSource> = null;
 
-    displayOpenFile: boolean = false;
+    displayOpenFile: boolean = true;
+
+    annotations$ = new BehaviorSubject<DocumentAnnotations>([]);
+    annotationProfile$ = new BehaviorSubject<Nullable<AnnotationProfile>>(null);
 
     constructor(private i18n: I18NService) {
         i18n.init();
-        this.TOOLBAR_CONFIG = DefaultToolbar.CONFIG;
+        this.setupAnnotations();
+        // configures the toolbar, esp. for a correct file opening and a correct switch between normal / accessible mode
+        this.TOOLBAR_CONFIG =
+            {
+            ...DefaultToolbar.CONFIG,
+            menu: {
+                ...DefaultToolbar.CONFIG.menu,
+                menuConfiguration: {
+                    menuItems: [
+                        {
+                            type: MenuItemType.ACTION,
+                            action: {
+                                ...DefaultActions.OPEN_FILE,
+                                handle: () => {
+                                    this.displayOpenFile = true;
+                                }
+                            }
+                        },
+                        ...DefaultToolbar.CONFIG.menu.menuConfiguration.menuItems.slice(1)
+                    ]
+                }
+            },
+            auxiliaryActions: [
+                ...(DefaultToolbar.CONFIG.auxiliaryActions as any),
+                ToolbarUtils.makeButton(SWITCH_MODE_ACTION(this.mode$))
+            ]
+        }
     }
 
-    protected readonly window = window;
+    ngOnInit(): void {
+        // This is doing all the wiring for the different events, that can happen when trying to open a file
+        this.viewerComponent.getViewer$().pipe().forEach(v => {
+            const wrapper = this.uploadDialogsWrapper;
 
+            v?.addErrorObserver({
+                complete(): void {
+                    v?.clearErrorObservers();
+                }, error(err: any): void {
+                    console.log("while trying to report an error, another error came on top: " + err);
+                }, next(error: any): void {
+                    let completeText = I18N.get().translateOnce("webtoolkitClient.document.documentLoadingError");
+                    if (error && error.className && error.className.toLowerCase().includes("exception")) {
+                        const message = error.message;
+                        if (message && (typeof message === "string")) {
+                            completeText += "\r\n" + message;
+                        }
+                    }
+                    wrapper.errorMessage = completeText;
+                    wrapper.displayErrorDialog = true;
+                }
+            });
+        });
+    }
+
+
+
+    private setupAnnotations() {
+        AnnotationProfileCache.get().loadAndCache(this.DEFAULT_PROFILE).then((profile) => {
+            this.annotationProfile$.next(profile.data);
+        });
+
+        this.annotations$.pipe(
+            tap(async (annos) => {
+                let annotationProfile = await AnnotationProfileUtils.getNewAnnotationProfile(annos, this.annotationProfile$.getValue());
+                if (annotationProfile) {
+                    this.annotationProfile$.next(annotationProfile);
+                }
+            })
+        ).subscribe();
+
+
+        AnnotationCustomizers.get().addCustomizer(a => {
+            if (a.getType() === AnnotationInstanceType.IMAGE) {
+                const anno = a.toDTO();
+                const typeRef = AnnotationProfileUtils.parseTypeId(anno.typeId);
+                if (typeRef.typeName === "JWTImageBordered") {
+                    (a as GWTImageAnnotationWrapper).setImageID("qrJWTAnnoImage");
+                } else if (typeRef.typeName === "JadiceImageBordered") {
+                    (a as GWTImageAnnotationWrapper).setImageID("qrJadiceAnnoImage");
+                }
+            }
+        });
+    }
+
+    setUrl(url: string) {
+        this.source = {
+            uri: url,
+            password: null
+        };
+    }
+
+    pageSelected(pageIndex: number) {
+        this.viewerComponent.setCurrentPageIndex(pageIndex);
+    }
+
+    loadDocWithPassword(password: string) {
+        const source = {...this.passwordRequiredSource, password} as DocumentSource;
+        this.passwordRequiredSource = null;
+        this.source = source;
+    }
+
+
+    async openFile(file: File) {
+        this.displayOpenFile = false;
+
+        this.uploadDialogsWrapper.openFile(file).then(s => {
+            if (s != null) {
+                this.source = s;
+            }
+        });
+    }
+
+    pickTemplateDoc(template: OpenFileTemplate) {
+        this.displayOpenFile = false;
+        this.source = {uri: template.data, password: null};
+    }
 }
---
+++ tutorial-001/server/src/main/java/org/jadice/jwv/tutorial/AnnoImageProvider.java
@@ -0,0 +1,39 @@
+/**
+ * <pre>
+ * Copyright (c), levigo holding gmbh.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ * </pre>
+ */
+package org.jadice.jwv.tutorial;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.stereotype.Component;
+
+import com.levigo.jadice.document.io.MemoryInputStream;
+import com.levigo.jadice.document.io.SeekableInputStream;
+import com.levigo.jadice.web.server.annotation.AnnotationImageProvider;
+
+@Component
+public class AnnoImageProvider implements AnnotationImageProvider {
+    @Override
+    public SeekableInputStream provideAnnotationImage(String annotationImageID) throws IOException {
+        InputStream is = switch (annotationImageID) {
+            case "signatureAnnoImage" ->
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("signature_kl_opaque_dotted.png");
+            case "qrJWTAnnoImage" ->
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("qr_jwt.png");
+            case "qrJadiceAnnoImage" ->
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("qr_jadice.png");
+            default -> null;
+        };
+
+        if (null == is)
+            throw new IOException("Couldn't find/load image for id \"" + annotationImageID + "\"");
+
+        return new MemoryInputStream(is);
+    }
+}
