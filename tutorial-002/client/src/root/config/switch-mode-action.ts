b303e47d63d4e18aecb10f9752456b17d7f342a1
anamariamarian1 <129378403+anamariamarian1@users.noreply.github.com>
2024-04-09 18:12:00 +0300

    feat(JWV-481): remove tutorial-002

--- tutorial-002/client/src/root/config/switch-mode-action.ts
+++
@@ -1,8 +0,0 @@
-import {DefaultActions} from "@levigo/webtoolkit-ng-client";
-import {JadiceIcon} from "@levigo/jadice-web-icons";
-import {BehaviorSubject} from "rxjs";
-
-// This action makes use of the default toggle action and switches between the "normal" and the "accessible" viewer mode
-export const SWITCH_MODE_ACTION  = (mode$ : BehaviorSubject<boolean>) => DefaultActions.Factories.makeToggleAction(
-    mode$, JadiceIcon.DEFAULT_READER_MODE, {content: "actions.accessibleMode", translate: true}
-);
